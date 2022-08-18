92a8359b147d223a0c4f0aaecd99e8e8b828b5fc
Christoph Parstorfer <christorfer@me.com>
2022-08-18 20:17:08 +0200

    Rename project to Music Control and Visualizer, Add App Clip target

---
+++ Music Control and Visualizer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Music Control and Visualizer
+//
+//  Created by Christoph Parstorfer on 18.08.22.
+//  Copyright © 2022 Christoph Parstorfer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music Control and Visualizer/Music_Control_and_VisualizerApp.swift
@@ -0,0 +1,18 @@
+//
+//  Music_Control_and_VisualizerApp.swift
+//  Music Control and Visualizer
+//
+//  Created by Christoph Parstorfer on 18.08.22.
+//  Copyright © 2022 Christoph Parstorfer. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Music_Control_and_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
